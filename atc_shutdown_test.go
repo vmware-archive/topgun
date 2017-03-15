bb23ef1baf1d45f4b51cc837dd529619b17fe083
Maria Shaldibina <mshaldibina@pivotal.io>
2017-03-15 12:43:02 -0700

    update shutdown test, remove sleep
    keep pended cause of blockers

--- atc_shutdown_test.go
+++ atc_shutdown_test.go
@@ -14,6 +14,7 @@ import (
 )
 
 var _ = XDescribe("[#137641079] ATC Shutting down", func() {
+	// pending on blockers for https://www.pivotaltracker.com/story/show/137641079
 	var dbConn *sql.DB
 
 	BeforeEach(func() {
@@ -30,24 +31,18 @@ var _ = XDescribe("[#137641079] ATC Shutting down", func() {
 		})
 
 		Describe("tracking builds previously tracked by shutdown ATC", func() {
-			var stopSession *gexec.Session
-
-			BeforeEach(func() {
-				By("stopping one of the web instances")
-				stopSession = spawnBosh("stop", "web/1")
-				Eventually(stopSession).Should(gexec.Exit(0))
-			})
-
-			AfterEach(func() {
-				<-stopSession.Exited
-			})
-
 			Context("with a build in-flight", func() {
 				var buildID string
+				var buildSession *gexec.Session
 
 				BeforeEach(func() {
+					By("stopping one of the web instances")
+					stopSession := spawnBosh("stop", "web/1")
+					<-stopSession.Exited
+					Eventually(stopSession).Should(gexec.Exit(0))
+
 					By("executing a task")
-					buildSession := spawnFly("execute", "-c", "tasks/wait.yml")
+					buildSession = spawnFly("execute", "-c", "tasks/wait.yml")
 					Eventually(buildSession).Should(gbytes.Say("executing build"))
 
 					buildRegex := regexp.MustCompile(`executing build (\d+)`)
@@ -63,25 +58,36 @@ var _ = XDescribe("[#137641079] ATC Shutting down", func() {
 					<-spawnFly("login", "-c", atcExternalURL2).Exited
 				})
 
-				AfterEach(func() {
-					restartSession := spawnBosh("start", "web/0")
-					<-restartSession.Exited
-					Eventually(restartSession).Should(gexec.Exit(0))
-				})
-
 				Context("when the atc tracking the build shuts down", func() {
-					JustBeforeEach(func() {
+					var trackedByWeb0 int
+					var waitingRegex *regexp.Regexp
+
+					BeforeEach(func() {
 						By("stopping the first web instance")
 						landSession := spawnBosh("stop", "web/0")
 						<-landSession.Exited
 						Eventually(landSession).Should(gexec.Exit(0))
+
+						waitingRegex = regexp.MustCompile("waiting for /tmp/stop-waiting")
+						trackedByWeb0 = len(waitingRegex.FindAllString(string(buildSession.Buffer().Contents()), -1))
+
+						restartSession := spawnBosh("start", "web/0")
+						<-restartSession.Exited
+						Eventually(restartSession).Should(gexec.Exit(0))
+					})
+
+					AfterEach(func() {
+						restartSession := spawnBosh("start", "web/0")
+						<-restartSession.Exited
+						Eventually(restartSession).Should(gexec.Exit(0))
 					})
 
 					It("continues tracking the build progress", func() {
 						By("waiting for another atc to attach to process")
 						watchSession := spawnFly("watch", "-b", buildID)
-						Eventually(watchSession).Should(gbytes.Say("waiting for /tmp/stop-waiting"))
-						time.Sleep(10 * time.Second)
+						Eventually(func() int {
+							return len(waitingRegex.FindAllString(string(watchSession.Buffer().Contents()), -1))
+						}, 1*time.Minute).Should(BeNumerically(">", trackedByWeb0))
 
 						By("hijacking the build to tell it to finish")
 						hijackSession := spawnFly(
